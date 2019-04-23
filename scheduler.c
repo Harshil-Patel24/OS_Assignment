25e895784c6aa23b260a1b57e707ce5a729767dc
Harshil Patel <harshilpatel.personal@gmail.com>
2019-04-23 20:22:07 +0800

    Scheduler started

--- scheduler.c
+++ scheduler.c
@@ -1,1 +1,21 @@
 #include "scheduler.h"
+
+int main( int argc, char** argv )
+{
+    if( argc == 2 )
+    {
+        int maxSize;
+
+        if( atoi( argv[1] ) != 0 )
+        {
+            maxSize = atoi( argv[1] );
+        }
+        else
+        {
+            perror( "Error: Usage- ./scheduler <ready queue size>" )
+        }
+
+        LinkedList readyQ = makeList( maxSize );
+    }
+    return 0;
+}
